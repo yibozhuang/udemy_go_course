4825b3df88d2c8e5a1d8e09b88508591ae6e642d
Yibo Zhuang <yibzhuang@gmail.com>
2017-09-02 21:01:56 -0700

    first go project

---
+++ helloworld/main.go
@@ -0,0 +1,12 @@
+// package main is special
+// package main means it is executable
+
+// other package names would make the code a library for other code to use
+package main
+
+// fmt is part of standard package
+import "fmt"
+
+func main() {
+	fmt.Println("Hi there!")
+}
